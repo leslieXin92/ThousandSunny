84b817acbea3222a3bd079cc7d80fbc039283c2d
leslieXin92 <leslieXin92@gmail.com>
2022-12-28 00:58:01 +0800

    feat: pinia持久化

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import { globalRegister } from './global'
 
 const pinia = createPinia()
+pinia.use(piniaPluginPersistedState)
 
 const app = createApp(App)
 app.use(router)
--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -4,30 +4,40 @@ import { ILoginParams } from '@/service/api/manage/type'
 import { login, logout } from '@/service/api/manage/manage'
 import { sessionCache } from '@/utils/cache'
 
-export const useUserStore = defineStore('user', () => {
-  type UserType = { username: string } | null
+export const useUserStore = defineStore(
+  'user',
+  () => {
+    type UserType = { username: string } | null
 
-  const user: Ref<UserType> = ref(null)
+    const user: Ref<UserType> = ref(null)
 
-  const isLogin = ref(false)
+    const isLogin = ref(false)
 
-  const handleLogin = async (userInfo: ILoginParams) => {
-    const { data } = await login(userInfo)
-    sessionCache.setCache('userInfo', data)
-    isLogin.value = true
+    const handleLogin = async (userInfo: ILoginParams) => {
+      const { data } = await login(userInfo)
+      user.value = data
+      sessionCache.setCache('userInfo', data)
+      isLogin.value = true
+    }
+
+    const handleLogout = async () => {
+      const userInfo = sessionCache.getCache('userInfo')
+      await logout(userInfo)
+      sessionCache.deleteCache('userInfo')
+      isLogin.value = false
+    }
+
+    return {
+      user,
+      isLogin,
+      handleLogin,
+      handleLogout
+    }
+  },
+  {
+    persist: {
+      key: 'userStore',
+      storage: sessionStorage
+    }
   }
-
-  const handleLogout = async () => {
-    const userInfo = sessionCache.getCache('userInfo')
-    await logout(userInfo)
-    sessionCache.deleteCache('userInfo')
-    isLogin.value = false
-  }
-
-  return {
-    user,
-    isLogin,
-    handleLogin,
-    handleLogout
-  }
-})
+)
