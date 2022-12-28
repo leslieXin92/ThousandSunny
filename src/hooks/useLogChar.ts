e778022aa6759fb4673d08dcfe00659244597c63
zhanglx <zhanglx@qianlong2.net>
2022-12-28 15:48:55 +0800

    feat: 骚一下之log艺术字

---
+++ src/hooks/useLogChar.ts
@@ -0,0 +1,5 @@
+import { leslie } from '@/assets/char'
+
+export const useLogChar = () => {
+  console.log(leslie)
+}
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createPinia } from 'pinia'
 import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import { globalRegister } from './global'
+import { useLogChar } from '@/hooks/useLogChar'
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedState)
@@ -13,3 +14,5 @@ app.use(router)
 app.use(pinia)
 app.use(globalRegister)
 app.mount('#app')
+
+useLogChar()
