bad42c75339bb6cfed1648301e3e53da6e8d7525
leslieXin92 <leslieXin92@gmail.com>
2023-01-06 02:17:17 +0800

    feat: three.js初体验

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,8 +1,15 @@
 <template>
   <h1>Home</h1>
+  <iframe :src='src' width='100%' height='100%'></iframe>
 </template>
 
 <script setup lang='ts'>
+import { onMounted, ref } from 'vue'
+
+const src = ref('')
+onMounted(() => {
+  src.value = './three/index.html'
+})
 </script>
 
 <style scoped lang='less'>
