28bd8c74fdc30abe010303e7956da8a2aaf5d395
leslie <leslieXin92@gmail.com>
2023-08-14 09:43:15 +0800

    chore: server config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0'
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
